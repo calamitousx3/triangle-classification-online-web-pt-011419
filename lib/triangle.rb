9a04643bec689ffd4de47ff372758d4c3b46f295
Carmen Renee <carmengenpt@gmail.com>
2019-02-25 13:29:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,10 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
   
   def initialize(side_1, side_2, side_3)
-    
+    @triangle_sides = []
+    @triangle_sides << side_1
+    @triangle_sides << side_2
+    @triangle_sides << side_3
   end  
   
   def kind 
